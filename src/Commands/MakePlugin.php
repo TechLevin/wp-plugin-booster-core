7e90b5cb9f6ae7289eab64f7fc8c05c24be05a42
Md Miraj Khan <miraj.net.xp@gmail.com>
2020-01-05 20:47:55 +0600

    add first command

--- src/Commands/MakePlugin.php
+++ src/Commands/MakePlugin.php
@@ -1,7 +1,22 @@
-<?php
+<?php namespace TechLevin\WPBooster\Core\Commands;
 
 
-class MakePlugin
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+
+
+class MakePlugin extends Command
 {
+    protected function configure()
+    {
+        $this->setName('make:plugin')
+             ->setDescription('Some Description')
+             ->setHelp('Some help')
+             ->addArgument('plugin-name', InputArgument::REQUIRED, 'Type plugin name');
+    }
 
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(sprintf('Hello World!, %s', $input->getArgument('plugin-name')));
+    }
 }
